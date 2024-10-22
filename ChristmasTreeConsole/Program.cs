77cac39f5b23ea41d3c8e39cb57fcce85c68c565
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-10-22 09:06:07 +0400

    Добавьте файлы проекта.

---
+++ ChristmasTreeConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿
+// track
+bool f =true;
+
+int h = 10;
+
+while (true)
+{
+
+    for (int i = 0; i < h; i++)
+    {
+       
+        for (int j = 0; j < h + i; j++)
+        {
+            if (j < h - 1 - i)
+                Console.Write("  ");
+            else
+            {
+                if (i != 0 && i != h - 1)
+                {
+
+                   
+                    if (j % 3 == 0 && i % 2 == 0)
+                    { 
+                        if(f)
+                        {
+                            Console.Write("0 ");
+                        }
+                        else
+                        {
+                            Console.Write("p ");
+                        }
+                        continue; 
+                    }
+                    else
+                    { Console.Write("* "); continue; }
+                }
+
+                Console.Write("* ");
+            }
+
+
+        }
+        Console.WriteLine();
+    }
+    Thread.Sleep(50);
+    Console.Clear();
+    f= !f; 
+   
+}
+
